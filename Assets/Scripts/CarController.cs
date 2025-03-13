de51f5411639869c0111d2a7c16764ebd470cf6d
NoahAboud <myg4617@autuni.ac.nz>
2025-03-13 20:17:06 +1300

    first

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour {
+
+    [SerializeField] WheelCollider frontRight;
+    [SerializeField] WheelCollider frontLeft;
+    [SerializeField] WheelCollider rearRight;
+    [SerializeField] WheelCollider rearLeft;
+
+    [SerializeField] Transform frontRightTransform;
+    [SerializeField] Transform frontLeftTransform;
+    [SerializeField] Transform rearLeftTransform;
+    [SerializeField] Transform rearRightTransform;
+
+
+    public float acceleration = 1000f;
+    public float breakingForce = 300f;
+    public float maxTurnAngle = 15f;
+
+    private float currentAcceleration = 0f;
+    private float currentBreakingForce = 0f;
+    private float currentTurnAngle = 0f;
+
+
+
+    private void FixedUpdate()
+    {
+        currentAcceleration = acceleration * Input.GetAxis("Vertical");
+
+
+        if (Input.GetKey(KeyCode.Space))
+            currentBreakingForce = breakingForce;
+        else
+            currentBreakingForce = 0f;
+
+        frontRight.motorTorque = currentAcceleration;
+        frontLeft.motorTorque = currentAcceleration;
+
+        frontRight.brakeTorque = currentBreakingForce;
+        frontLeft.brakeTorque = currentBreakingForce;
+        rearRight.brakeTorque = currentBreakingForce;
+        rearLeft.brakeTorque = currentBreakingForce;
+
+
+        currentTurnAngle = maxTurnAngle * Input.GetAxis("Horizontal");
+        frontLeft.steerAngle = currentTurnAngle;
+        frontRight.steerAngle = currentTurnAngle;
+
+        UpdateWheel(frontLeft, frontLeftTransform);
+        UpdateWheel(frontRight, frontRightTransform);
+        UpdateWheel(rearLeft, rearLeftTransform);
+        UpdateWheel(rearRight, rearRightTransform);
+
+    }
+
+    void UpdateWheel (WheelCollider col, Transform trans)
+    {
+        Vector3 position;
+        Quaternion rotation;
+        col.GetWorldPose(out position, out rotation);
+
+        trans.position = position;
+        trans.rotation = rotation;
+    }
+}
